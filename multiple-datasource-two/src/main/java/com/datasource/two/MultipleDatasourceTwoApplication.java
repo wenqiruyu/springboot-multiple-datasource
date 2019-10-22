fb1dc650a54c079a8ccbd5171fe3c4ab1fa114ff
Allen <yingxing.liu1@cignacmb.com>
2019-10-22 17:22:51 +0800

    添加日志功能

--- multiple-datasource-two/src/main/java/com/datasource/two/MultipleDatasourceTwoApplication.java
+++ multiple-datasource-two/src/main/java/com/datasource/two/MultipleDatasourceTwoApplication.java
@@ -1,13 +1,19 @@
 package com.datasource.two;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MultipleDatasourceTwoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MultipleDatasourceTwoApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(MultipleDatasourceTwoApplication.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("MultipleDatasourceTwoApplication is begin ...");
+        SpringApplication.run(MultipleDatasourceTwoApplication.class, args);
+    }
 
 }
